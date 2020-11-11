0777bd56faed8e9d0c86650d079d326abc2b8b07
SFGamink <72718264+SFGamink@users.noreply.github.com>
2020-11-11 11:08:23 +0700

    Create t00l.py

---
+++ t00l.py
@@ -0,0 +1,101 @@
+#!/usr/bin/bash
+#hese asww:)
+
+#warna
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+clear
+python2 password.py
+clear
+toilet -f pagga "SFGamink"| lolcat
+sleep 1
+clear
+toilet -f pagga "Telegram"| lolcat
+clear
+termux-open-url https://t.me/exoProKiKi
+sleep 5
+toilet -f pagga "Whatsapp"|lolcat
+clear
+termux-open-url hhttps://wa.me/6285659032860
+sleep 5
+neofetch
+echo "╔══════════════════════════════════════╗"
+toilet -f pagga " ANONYMOUS"| lolcat
+toilet -f pagga " INDONESIA"| lolcat
+echo "╚══════════════════════════════════════╝"
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+toilet -f pagga "SFGamink"| lolcat
+echo $purple "
+████████╗░█████╗░░█████╗░██╗░░░░░░██████╗
+╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝
+░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗
+░░░██║░░░██║░░██║██║░░██║██║░░░░░░╚═══██╗
+░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╔╝ 
+░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░
+\033[1;93m╔════════════════════════════════════════════╗
+\033[1;93m║\033[1;96m* \033[1;93mAuthor  \033[1;93m: \033[1;93mSurya Gumawang  \033[1;93m               ║
+\033[1;93m║\033[1;96m* \033[1;93mGitHub  \033[1;93m: \033[1;93m\033[4mhttps://github.com/SFGamink\033[0m \033[1;93m  ║
+\033[1;93m╚════════════════════════════════════════════╝"
+echo ""
+echo "╔══════════════════════════════╗"
+toilet -f pagga "   MENU TOOLS!!!"| lolcat
+echo "╚══════════════════════════════╝"
+echo $cy "
+┏━━━━━┫{menu SFGamink Tools}┣━━━━━┓
+┃
+┠──[1] Camera CCTV ✔
+┃
+┠──[2] HACK FB(Tanpa Login) ✔
+┃
+┠──[3] SPAM C4LL ✔
+┃
+┗────[99] exit ✘"
+echo '\033[35;1m'
+read -p "root@Pilih Nomor > " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+pkg install git
+git clone  https://github.com/SFGamink/sia
+cd sia
+python3 camera.py
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+pkg install git
+git clone https://github.com/SFGamink/surya
+cd surya
+python2 uya.py
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+pkg install git
+git clone https://github.com/SFGamink/C4LL
+cd C4LL
+python2 c4ll.py
+fi
+
+if [ $bro = 99 ] || [ $bro = 99 ]
+then
+echo $cyan  "NUHUN ATOS MAKE TOOLS IEU "
+sleep 1
+exit
+fi
