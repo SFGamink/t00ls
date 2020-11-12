a73250cdc990927432548068cefdaa38c9010095
SFGamink <72718264+SFGamink@users.noreply.github.com>
2020-11-12 14:51:14 +0700

    Update t00l.py

--- t00l.py
+++ t00l.py
@@ -7,15 +7,15 @@ def main():
     time.sleep(1)
     os.system ('clear')
     print '[96m'
-    os.system ('figlet Hacking Hub')
+    os.system ('figlet TOOL')
     print'[92m=================================='
     print'[97m Tools   :[96mSFGamink '
     print'[97m Author  :[96mSurya Gumawang '
     print'[92m================================='
     print'[92m++++++++++++ [97mTOOLS [92m++++++++++++'
     print'[92m[[97m1[92m] [97mCAMERA CCTV '
-    print'[92m[[97m2[92m [98mFB TANPA LOGIN '
-    print'[92m[[97m3[92m] [99mC4LL '
+    print'[92m[[97m2[92m [97mFB TANPA LOGIN '
+    print'[92m[[97m3[92m] [97mC4LL '
     print'[92m[[97m4[92m] [97mSantet Online '
     print'[92m[[91m0[92m] [91mExit '
     gans = raw_input ('[97m=SFGAMINK=>[93m ')
@@ -25,13 +25,13 @@ def main():
         os.system ('pkg install python3')
         os.system ('pip3 install requests')
         os.system ('pip3 install colorama')
-        print ('[97m$ Ketik [93mcd sia')
-        print ('[97m$ Terus[93m python3 cctv.py')
+        print ('[97m Ketik [93mcd sia')
+        print ('[97m Terus[93m python3 cctv.py')
     if gans in ['2']:
         time.sleep(1)
         os.system ('git clone https://github.com/SFGamink/surya')
-        print ('[97m$ Ketik[93mcd surya')
-        print ('[97m$ Terus[93m python2 uya.py')
+        print ('[97m Ketik[93mcd surya')
+        print ('[97m Terus[93m python2 uya.py')
     if gans in ['3']:
         time.sleep(1)
         os.system ('git clone https://github.com/SFGamink/C4LL')
@@ -40,14 +40,13 @@ def main():
         os.system ('gem install ruby')
         os.system ('pkg install php')
         os.system ('pip install bash')
-        print ('[97m$ Ketik [93mcd C4LL')
-        print ('[97m$ Terus[93m python c4ll.py')
+        print ('[97m Ketik [93mcd C4LL')
+        print ('[97m Terus[93m python c4ll.py')
      if gans in ['4']:
         time.sleep(1)
-        print ('[97m$ Yahaha Kena Upit:)')
-        print ('[97m$ Emang Enak :v')
-        print ('[97m$ Tunggu Updatetan Lah Bangsa(t)')
-        print ('[97m$ Cape Bor Kieu Teh Tong Sok Ngagampangkeun!')    
+        print ('[97m Yahaha Kena Upit')
+        print ('[97m Tunggu Updatetan Lah Bangsui')
+        print ('[97m Cape Bor Kieu Teh Tong Sok Ngagampangkeun!')    
     else:
         time.sleep(1)
         print '[97m Pilih CING Bener [92mNJIRR . . .'
