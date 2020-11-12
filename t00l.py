a3f8ac87f52456d99784f8d61f0c19c4a2668976
SFGamink <72718264+SFGamink@users.noreply.github.com>
2020-11-12 07:07:12 +0700

    Update t00l.py

--- t00l.py
+++ t00l.py
@@ -1,101 +1,119 @@
-#!/usr/bin/bash
-#hese asww:)
-
-#warna
-blue='\033[34;1m'
-green='\033[32;1m'
-purple='\033[35;1m'
-cyan='\033[36;1m'
-red='\033[31;1m'
-white='\033[37;1m'
-yellow='\033[33;1m'
-
 clear
-python2 password.py
+b="\033[1m"
+u="\033[4m"
+bl="\033[30m"
+r="\033[31m"
+g="\033[32m"
+bu="\033[34m"
+m="\033[35m"
+c="\033[36m"
+w="\033[37m"
+endc="\033[0m"
+enda="\033[0m"
+blue="\033[1;34m"
+cyan="\033[1;36m"
+red="\033[1;31m"
+figlet "Surya Gumawang"
+echo "Selamat datang di tools kami" |lolcat
+echo $c " ========================================== ${endc}";
+echo $c "  ████████╗░█████╗░░█████╗░██╗░░░░░░██████╗ ${endc}";
+echo $c "  ╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝ ${endc}";
+echo $c "  ░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗░ ${endc}";
+echo $c "  ░░░██║░░░██║░░██║██║░░██║██║░░░░░░╚═══██╗ ${endc}";
+echo $c "  ░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╔╝ ${endc}";
+echo $c "  ░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░ ${endc}";
+echo $c " ========================================== ${endc}";
+
+echo  "[+]===========================================[+]" |lolcat
+echo  "[+] Tools     : SFGamink $white               [+] " |lolcat
+echo  "[+] Author   : Surya Gumawang $white          [+] " |lolcat
+echo  "[+]===========================================[+]" |lolcat
+
+echo $r"•••••••••••••••••••••••••••••••••••••••••••••${endc}";
+echo   "##### Kumpulan Tools SFGamink #####" |lolcat
+echo $r"•••••••••••••••••••••••••••••••••••••••••••••${endc}";
+
+trap ctrl_c INT
+ctrl_c() {
 clear
-toilet -f pagga "SFGamink"| lolcat
+echo  $red"[#]> (Ctrl + C ) Detected,
+Trying To Exit ... "
+echo  $cyan"[#]> SUNDANESE"
 sleep 1
-clear
-toilet -f pagga "Telegram"| lolcat
-clear
-termux-open-url https://t.me/exoProKiKi
-sleep 5
-toilet -f pagga "Whatsapp"|lolcat
-clear
-termux-open-url hhttps://wa.me/6285659032860
-sleep 5
-neofetch
-echo "╔══════════════════════════════════════╗"
-toilet -f pagga " ANONYMOUS"| lolcat
-toilet -f pagga " INDONESIA"| lolcat
-echo "╚══════════════════════════════════════╝"
-bi='\033[34;1m' #biru
-ij='\033[32;1m' #ijo
-pr='\033[35;1m' #purple
-cy='\033[36;1m' #cyan
-me='\033[31;1m' #merah
-pu='\033[37;1m' #putih
-ku='\033[33;1m' #kuning
-or='\033[1;38;5;208m' #Orange
-toilet -f pagga "SFGamink"| lolcat
-echo $purple "
-████████╗░█████╗░░█████╗░██╗░░░░░░██████╗
-╚══██╔══╝██╔══██╗██╔══██╗██║░░░░░██╔════╝
-░░░██║░░░██║░░██║██║░░██║██║░░░░░╚█████╗
-░░░██║░░░██║░░██║██║░░██║██║░░░░░░╚═══██╗
-░░░██║░░░╚█████╔╝╚█████╔╝███████╗██████╔╝ 
-░░░╚═╝░░░░╚════╝░░╚════╝░╚══════╝╚═════╝░
-\033[1;93m╔════════════════════════════════════════════╗
-\033[1;93m║\033[1;96m* \033[1;93mAuthor  \033[1;93m: \033[1;93mSurya Gumawang  \033[1;93m               ║
-\033[1;93m║\033[1;96m* \033[1;93mGitHub  \033[1;93m: \033[1;93m\033[4mhttps://github.com/SFGamink\033[0m \033[1;93m  ║
-\033[1;93m╚════════════════════════════════════════════╝"
 echo ""
-echo "╔══════════════════════════════╗"
-toilet -f pagga "   MENU TOOLS!!!"| lolcat
-echo "╚══════════════════════════════╝"
-echo $cy "
-┏━━━━━┫{menu SFGamink Tools}┣━━━━━┓
-┃
-┠──[1] Camera CCTV ✔
-┃
-┠──[2] HACK FB(Tanpa Login) ✔
-┃
-┠──[3] SPAM C4LL ✔
-┃
-┗────[99] exit ✘"
-echo '\033[35;1m'
-read -p "root@Pilih Nomor > " bro
+echo  $white"[#]> semoga bermanfaat :)"
+sleep 1
+exit
+}
+lagi=1
+while [ $lagi -lt 6 ];
+do
+sleep 1
+echo ""
+echo  "=========================================" |lolcat
+echo  $r "1.  CAMERA CCTV                        [÷]${endc}";
+echo  $g "2.  FB MASSAL TANPA LOGIN              [÷]${endc}";
+echo  $c "3.  C4LL                               [÷]${endc}";
+echo  $r "00. Exit                               [÷]${endc}";
+echo  "=========================================" |lolcat
+echo ""
+echo  "╭─SFGamink" |lolcat
+read -p "╰─#" pil;
 
-if [ $bro = 1 ] || [ $bro = 1 ]
-then
+#CAMERA CCTV
+
+1) apt update && apt upgrade
 clear
-pkg install git
-git clone  https://github.com/SFGamink/sia
+figlet "SFGamink"
+pkg install python3
+pip3 install requests
+pip3 install colorama
+git clone https://github.com/SFGamink/sia
 cd sia
-python3 camera.py
-fi
+python3 cctv.py
 
-if [ $bro = 2 ] || [ $bro = 2 ]
-then
+;;
+
+#FB MASSAL TANPA LOGIN
+
+2) apt update && apt upgrade
 clear
-pkg install git
+figlet "SFGamink"
+pkg install python2
 git clone https://github.com/SFGamink/surya
 cd surya
 python2 uya.py
-fi
 
-if [ $bro = 3 ] || [ $bro = 3 ]
-then
+;;
+
+#C4LL
+
+3) apt update && apt upgrade
 clear
+figlet "SFGamink"
+pkg install python
+pip install requests
+gem install ruby
+pkg install php
+pip install bash
 pkg install git
 git clone https://github.com/SFGamink/C4LL
 cd C4LL
 python2 c4ll.py
-fi
 
-if [ $bro = 99 ] || [ $bro = 99 ]
-then
-echo $cyan  "NUHUN ATOS MAKE TOOLS IEU "
-sleep 1
+;;
+
+00) clear
+figlet "SFGamink" |lolcat
+echo "Good bye" |lolcat
+echo "Jangan Kapok Untuk Memakai Tools Ini" |lolcat
 exit
-fi
+
+;;
+
+*) echo  $g "SALAH!!!,
+EWH PILIHANNA!
+TUNGGUAN UPDATENA
+esac
+done
+done
