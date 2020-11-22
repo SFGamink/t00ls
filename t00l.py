a83c1825b34c30ca4830d497ca7164ebd298520e
SFGamink <72718264+SFGamink@users.noreply.github.com>
2020-11-22 10:18:07 +0700

    Update t00l.py

--- t00l.py
+++ t00l.py
@@ -1,57 +1,220 @@
-#Coded By SFGamink
+clear
+	bi='\033[34;1m' #biru
+	i='\033[32;1m' #ijo
+	pur='\033[35;1m' #purple
+	cy='\033[36;1m' #cyan
+	me='\033[31;1m' #merah
+	pu='\033[37;1m' #purple
+	ku='\033[33;1m' #kuning
+	Lightred='\033[91m' #Merah Cahaya
+	BrightBlue='\033[34m' #Cahaya Biru
+	echo
+	echo
+	python2 sploit.py
+	echo
+	clear
+	echo
+	echo $blue"          ======================+++++++++++==================="
+	echo $red"            Sellow Beb Lagi Nginstall Nih Tunggu ya Say..."   
+	sleep 1
+	echo '''\a
+	\033[34;1m                              _
+	\033[32;1m                             / |
+	\033[35;1m                             | |
+	\033[36;1m                             | |
+	\033[37;1m                            _|_|_ '''
+	sleep 1
+	apt update && apt upgrade
+	apt install nano
+	apt install git
+	pkg install python python2 vim figlet curl
+	clear
+	echo
+	echo
+	echo
+	echo
+	echo $blue"         ======================+++++++++++==================="
+	echo $red"            Jika Anda Gans Maka Proses Installasi akan cepat"
+	sleep 1
+	echo '''\a
+	\033[32;1m                             ____
+	\033[34;1m                            |___ \
+	\033[35;1m                             __) |
+	\033[36;1m                            / __/
+	\033[37;1m                           |_____| '''
+	sleep 1
+	apt install php
+	pip2 install mechanize
+	pip2 install lolcat
+	pip2 install requests
+	clear
+	echo
+	echo
+	echo
+	echo
+	echo $blue"         ======================+++++++++++==================="
+	echo $red"            Semakin Gans Semakin Cepat Masuk ke Menu"
+	sleep 1
+	echo '''\a
+	\033[35;1m                            _____
+	\033[36;1m                           |___ /
+	\033[37;1m                             |_ \
+	\033[34;1m                            ___) |
+	\033[32;1m                           |____/ '''
+	sleep 1
+	clear
+	echo
+	echo
+	echo
+	echo
+	echo $blue"         ======================+++++++++++==================="
+	echo $red"            Btw Jika Anda Cewe hubungi :081316577616"
+	sleep 1
+	echo '''\a
+	\033[33;1m                            _  _
+	\033[34;1m                           | || |
+	\033[35;1m                           | || |_
+	\033[36;1m                           |__   _|
+	\033[37;1m                              |_| '''
+	sleep 1
+	clear
+	echo
+	echo
+	echo
+	echo
+	echo $white"         Prosses failed to go to the Main Menu"
+	echo $red"            F For Respect"
+	sleep 1
+	echo '''\a
+	\033[36;1m                             ____
+	\033[35;1m                            | ___|
+	\033[34;1m                            |___ \
+	\033[33;1m                             ___) |
+	\033[32;1m                            |____/ '''
+	sleep 1
+	clear
+	echo
+	echo $pur"==================="$cy" ============================="
+	echo $i"##"$me"  AUTHOR  :  Surya Gumawang "$i"               ##"
+	echo $i"##"$pu"  WhatsApp:  085659032860"$i"                  ##"
+	echo $i"##"$me"  Blogger :  permulaanmyblog.bogspot.com"$i"   ##"
+	echo $i"##"$pu"  TEAM    :  SFGamink"$i"                      ##"
+	echo $pur"==================="$cy" ============================="
+	echo $i"[" $me" INI TOOLS GERATIS GAYN", $bi"DILARANG KERAD MENJUALNYA KE PIHAK LAINNYA" $i"]"
+	echo $i"[" $me" TOOLS FREE USE, NO NEED TO PAYS" $pu"!!!" $i"]"
+	echo #pu"~~~~~~~~~~~~~~~~~~~~~"$me"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	echo
+	echo $cy"SFGamink Menu:"
+	echo
+    echo $me"[==============="$pu"===============]"
+	echo $bi"["$pu"1"$bi"]"$i" Dark FB"
+	echo $me"[==============="$pu"===============]"
+	echo $bi"["$pu"2"$bi"]"$i" Camera CCTV"
+	echo $me"[==============="$pu"===============]"
+	echo $me"["$pu"3"$me"]"$i" FB"
+	echo $me"[==============="$pu"===============]"
+	echo $bi"["$pu"4"$bi"]"$i" C4LL"
+	echo $me"[==============="$pu"===============]"
+	echo $bi"["$pu"5"$bi"]"$i" Santet Online"
+    echo $me"[==============="$pu"===============]"
+	echo $me"["$bi"00"$i"]"$me" EXIT"
+	echo
+	echo $pur"SFGamink["$pur"Pilih Ayeuna"$pur"]"
+	read -p"SFGamink
+    
+    if [ $pil = 1 ]
+	then
+	clear
+	figlet -f slant "SAKEUDENG"|lolcat
+	sleep 1
+	pkg install git python2
+	pip2 install --upgrade pip
+	git clone https://github.com/SFGamink/gelap
+	echo -e "${y} USERNAME:SFGamink"
+	echo -e "${y} PASSWORD:#Sukaraja123"
+	echo -e "${y} Ketik cd gelap"
+	echo -e "${y} Terus python2 padam.py"
+	fi
+	
 
-import os,sys,time
+	if [ $pil = 2 ]
+	then
+	clear
+	figlet -f slant "SAKEUDENG"|lolcat
+	sleep 1
+	pkg install python
+	pkg install git 
+	pkg install python3
+	pip3 install requests
+    pip3 install colorama
+    git clone https://github.com/SFGamink/sia
+	echo -e "${y} USERNAME:SFGamink"
+	echo -e "${y} PASSWORD:#Sukaraja123"
+	echo -e "${y} Ketik cd sia"
+	echo -e "${y} Terus python3 cctv.py"
+	fi
+	
 
+	if [ $pil = 3 ]
+	then
+	clear
+	figlet -f slant "SAKEUDENG"|lolcat
+	sleep 1
+	pkg install pyton2
+	pkg install git
+	git clone https://github.com/SFGamink/surya
+	echo -e "${y} USERNAME:SFGamink"
+	echo -e "${y} PASSWORD:#Sukaraja123"
+	echo -e "${y} Ketik cd surya"
+	echo -e "${y} Terus python2 uya.py"
+	fi
+	
 
-def main():
-    time.sleep(1)
-    os.system ('clear')
-    print '[96m'
-    os.system ('figlet TOOL')
-    print'[92m=================================='
-    print'[97m Tools   :[96mSFGamink '
-    print'[97m Author  :[96mSurya Gumawang '
-    print'[92m================================='
-    print'[92m++++++++++++ [97mTOOLS [92m++++++++++++'
-    print'[92m[[97m1[92m] [97mCAMERA CCTV '
-    print'[92m[[97m2[92m [97mFB TANPA LOGIN '
-    print'[92m[[97m3[92m] [97mC4LL '
-    print'[92m[[97m4[92m] [97mSantet Online '
-    print'[92m[[91m0[92m] [91mExit '
-    gans = raw_input ('[97m=SFGAMINK=>[93m ')
-    if gans in ['1']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/SFGamink/sia')
-        os.system ('pkg install python3')
-        os.system ('pip3 install requests')
-        os.system ('pip3 install colorama')
-        print ('[97m Ketik [93mcd sia')
-        print ('[97m Terus[93m python3 cctv.py')
-    if gans in ['2']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/SFGamink/surya')
-        print ('[97m Ketik[93mcd surya')
-        print ('[97m Terus[93m python2 uya.py')
-    if gans in ['3']:
-        time.sleep(1)
-        os.system ('git clone https://github.com/SFGamink/C4LL')
-        os.system ('pkg install python')
-        os.system ('pip install requests')
-        os.system ('gem install ruby')
-        os.system ('pkg install php')
-        os.system ('pip install bash')
-        print ('[97m Ketik [93mcd C4LL')
-        print ('[97m Terus[93m python c4ll.py')
-     if gans in ['4']:
-        time.sleep(1)
-        print ('[97m Yahaha Kena Upit')
-        print ('[97m Tunggu Updatetan Lah Bangsui')
-        print ('[97m Cape Bor Kieu Teh Tong Sok Ngagampangkeun!')    
-    else:
-        time.sleep(1)
-        print '[97m Pilih CING Bener [92mNJIRR . . .'
-        time.sleep(1)
-        main()
-
-main()
- 
+	if [ $pil = 4 ]
+	then
+	clear
+	figlet -f slant "SAKEUDENG"|lolcat
+	sleep 1
+    pkg install python
+    pkg install python2
+    pip install requests
+    gem install ruby
+    pkg install php
+    pip install bash
+    pkg install git
+	git clone https://github.com/SFGamink/C4LL
+	echo -e "${y} USERNAME:SFGamink"
+	echo -e "${y} PASSWORD:#Sukaraja123"
+	echo -e "${y} Ketik cd C4LL"
+	echo -e "${y} Terus python c4ll.py"
+	fi
+    
+    if [ $pil = 5 ]
+	then
+	clear
+	figlet -f slant "Maaf Tools ini Sedang Close"|lolcat
+	sleep 1
+	echo -e "${y} MAU AJA DITIPU"
+	echo -e "${y} TUNGGUIN UPDATE NYA LAH"
+	echo -e "${y} ENTAR BEBERAPA BULAN LGI"
+	echo -e "${y} TUNGGUAN WEH"
+	fi
+    
+    if [ $pil = 00 ]
+	then
+	clear
+	figlet -f slant "SAKEUDENG"|Ulolcat
+	sleep 2
+	echo $cy"NUHUN TOS GUNAKEUN ALAT IEU"
+	sleep 2
+	echo $i"GUNAKEUN CING BENER"
+	sleep 2
+	echo $pur"MUN AYA KASALAHAN CHAT IEU"
+	sleep 2
+	echo $ku"WAName   :"$i" SURYA GUMAWANG"
+	echo $ku"Blogger  :"$i" https://permulaanmyblog.blogspot.com"
+	echo $ku"WhatsApp :"$i" 085659032860"
+	sleep 2
+	echo $pur"NUHUN"
+	exit
+	fi
